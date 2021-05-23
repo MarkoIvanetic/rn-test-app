8741a71c508e86601cb58015557085c1f5fa48d7
MarkoIvanetic <manna.grad@gmail.com>
2021-05-23 19:04:11 +0200

    part 33

--- App.js
+++ App.js
@@ -1,65 +1,97 @@
-import React, { useState } from 'react';
-import uuid from 'react-native-uuid';
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import React, { useState } from 'react'
+import uuid from 'react-native-uuid'
+import { StatusBar } from 'expo-status-bar'
+import { Button, AsyncStorage, StyleSheet, Text, TextInput, View, ScrollView, FlatList } from 'react-native'
+
+const generateItem = (n) => {
+	return (new Array(n).fill(undefined)).map((arb, i) => {
+		return {
+			id: uuid.v4(),
+			text: "Test item #"+i
+		}
+	})
+}
 
 export default function App() {
-	const [items, setItems] = useState([]);
-	const [newName, setNewName] = useState('');
+	const [items, setItems] = useState(generateItem(10))
+	const [newName, setNewName] = useState('')
 
 	const createItem = () => {
 		if (!newName) {
-			return;
+			return
 		}
-		setItems([...items, { id: uuid.v4(), text: newName }]);
-		setNewName('');
-	};
+		setItems([...items, { id: uuid.v4(), text: newName }])
+		setNewName('')
+	}
+
+
 
 	return (
 		<View style={styles.container}>
 			<View>
-				{items.map(item => {
-					return (
-						<View style={styles.item}>
-							<Text>{item.text}</Text>
-							<Button
-								style={styles.removeBtn}
-								title='X'
-								onPress={() => {
-									setItems(items.filter(i => i.id !== item.id));
-								}}></Button>
-						</View>
-					);
-				})}
-				<TextInput
-					style={styles.input}
-					placeholder='Enter text'
-					onChangeText={setNewName}
-					value={newName}
-				/>
+								<TextInput style={styles.input} placeholder='Enter text' onChangeText={setNewName} value={newName} />
 				<Button title='ADD' disabled={!newName} onPress={createItem}></Button>
+				<FlatList
+					data={items}
+					style={styles.itemList}
+					renderItem={({item}) => {
+						console.log(item);
+						return (
+							<View style={styles.item}>
+								<Text style={styles.itemText}>{item.text}</Text>
+								<Button
+									style={styles.removeBtn}
+									title='X'
+									onPress={() => {
+										setItems(items.filter(i => i.id !== item.id))
+									}}></Button>
+							</View>
+						)
+					}}></FlatList>
+
 			</View>
 		</View>
-	);
+	)
 }
 
 const styles = StyleSheet.create({
 	container: {
 		padding: 30,
-		marginTop: 60,
+		paddingBottom: 60,
+		marginTop: 60
 	},
 	input: {
 		height: 40,
+		padding: 10,
 		borderBottomColor: 'black',
 		marginBottom: 10,
 		borderBottomWidth: 1,
 	},
+	itemList: {
+		 backgroundColor: 'rgba(210, 240, 240, 0.5)',
+		 marginBottom: 120
+	},
 	item: {
+		backgroundColor: 'rgb(210, 240, 240)',
 		flexDirection: 'row',
+		alignItems: 'center',
+		marginTop: 10,
+		padding: 10,
+		paddingTop: 5,
+		paddingBottom: 5,
+		maxWidth: 400,
+		marginBottom: 10,
+		borderWidth: 1,
+		borderColor: '#333',
+	},
+	itemText: {
+		flex: 1,
 	},
 	removeBtn: {
-		width: 40,
+		width: 20,
+		height: 20,
+		borderRadius: 5,
 	},
-});
+})
 
 // <Text>{outputText}</Text>
